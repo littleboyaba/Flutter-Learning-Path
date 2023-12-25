6929c34332d943c7be9a4df85648cea4c660b04c
littleboyaba <northlightingstar@gmail.com>
2023-12-26 00:09:44 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Home',
-      home: HomeScreen(),
-    );
+    return const MaterialApp(home: HomeActivity(),);
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
-      body: Center(
-         child: Text('Hello Flutter'),
-      ),
+      appBar: AppBar(title: Text("My App"), backgroundColor: Colors.blue,),
+      body: Text("Hello"),
     );
   }
 }
